e486d5175c06683fb1b42dcdef1023621335bb11
Len Damico <len@lendami.co>
2025-11-04 21:33:22 -0500

    added date filtering

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,12 @@
+// Filters
+const dateFilter = require('./src/filters/date-filter.js');
+const w3DateFilter = require('./src/filters/w3-date-filter.js');
+
 module.exports = (config) => {
+	// Add filters
+	config.addFilter('dateFilter', dateFilter);
+	config.addFilter('w3DateFilter', w3DateFilter);
+	
 	// Define globs for each content type
 	const postsGlob     = "./src/posts/*.md";
 	const notesGlob     = "./src/notes/*.md";
